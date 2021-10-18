742e44c343e57c8096c3354f898875425e8a0eb0
BenJThornton <91638449+BenJThornton@users.noreply.github.com>
2021-10-17 21:46:48 -0700

    commit

---
+++ loop_challenge.js
@@ -0,0 +1,38 @@
+/* challenge 1 */
+
+for(i= 1; i< 20; i++){
+    if(i % 2 == 1){
+        console.log(i)
+    }
+}
+
+/* challenge 2 */
+
+for(i = 100; i > 0; i--){
+    if(i % 3 == 0){
+        console.log(i)
+    }
+}
+
+/* challenge 3 */
+
+var arr = [4, 2.5, -0.5, -2, -3.5]
+for(i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+
+/*challenge 4 */
+
+var sum = 0
+for(i = 1; i <= 100; i++){
+    sum += i
+}
+console.log(sum)
+
+/* challenge 5 */
+
+var product = 0
+for(i = 1; i <+ 12; i++){
+    product += i
+}
+console.log(product)
